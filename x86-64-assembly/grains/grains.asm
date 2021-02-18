d0083984bb9fbdbf0e882db0bdcfbe31eb07162a
loveJesus <loveJesus@loveJesus.xyz>
2021-02-18 05:01:10 -0600

    HALLELUJAH - grains and isogram asm

--- x86-64-assembly/grains/grains.asm
+++ x86-64-assembly/grains/grains.asm
@@ -1,10 +1,25 @@
+; For God so loved the world, that He gave 
+; His only begotten Son, that all who believe in 
+; Him should not perish but have everlasting life.
 section .text
+;extern uint64_t square(int64_t number);
 global square
 square:
-    ; Provide your implementation here
+    dec rdi
+    cmp rdi, 63
+    ja _square_err_aleluya
+    mov rax, 1
+    mov ecx, edi
+    shl rax, cl
     ret
 
+_square_err_aleluya:
+    mov eax, 0
+    ret
+
+;extern uint64_t total(void);
 global total
 total:
-    ; Provide your implementation here
+    mov rax, -1 ;64squares = 64bit - 1
     ret
+    
--- x86-64-assembly/isogram/isogram.asm
+++ x86-64-assembly/isogram/isogram.asm
@@ -1,5 +1,28 @@
+; ללויה
 section .text
+; Ascii 65<=upper<=90 97<=lower<=122 
+;extern int is_isogram(const char *str_aleluya);
 global is_isogram
 is_isogram:
-    ; Provide your implementation here
+    xor edx, edx ;hit buffer aleluya    
+_is_isogram_loopleluya:
+    movzx ecx, byte [rdi]
+    test cl, cl
+    je _is_isogram_eos_aleluya
+    inc rdi
+    and cl, 0b1101_1111 ;Make uppercase aleluya
+    sub cl, 65    
+    cmp cl, 26
+    ja _is_isogram_loopleluya    
+    bts edx, ecx
+    jnc _is_isogram_loopleluya
+    xor rax, rax ; not isogram
     ret
+
+_is_isogram_eos_aleluya:
+    mov rax, 1
+    ret
+
+; For God so loved the world, that He gave 
+; His only begotten Son, that all who believe in 
+; Him should not perish but have everlasting life.
