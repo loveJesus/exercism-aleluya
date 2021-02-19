6c72f98cd42fd6d526b89c878e0d3dcf75866ba3
loveJesus <loveJesus@loveJesus.xyz>
2021-02-18 18:19:33 -0600

    HALLELUJAH rotational-cipher and binary-search asm

--- x86-64-assembly/binary-search/binary_search.asm
+++ x86-64-assembly/binary-search/binary_search.asm
@@ -5,4 +5,33 @@ section .text
 ;extern int find(int *array_aleluya, int size_aleluya, int value_aleluya);
 global find
 find:
+    test rsi, rsi
+    je _find_error_aleluya ; zero size aleluya 
+    mov r8, 0         ; ALELUYA hold possible array extents
+    mov r9, rsi  
+    
+_find_loop_aleluya:
+    mov rax, r9
+    sub rax, r8
+    shr rax, 1
+    add rax, r8
+   
+    mov ecx, [rdi + rax * 4]
+    cmp ecx, edx
+    je _find_found_aleluya    
+    jg _find_gt_aleluya
+    cmp rax, r8
+    jle _find_error_aleluya ; not in array    
+    mov r8, rax
+    jmp _find_loop_aleluya
+
+_find_gt_aleluya:
+    cmp rax, r9
+    jge _find_error_aleluya ; not in array 
+    mov r9, rax
+    jmp _find_loop_aleluya
+
+_find_error_aleluya:
+    mov rax, -1
+_find_found_aleluya:
     ret
--- x86-64-assembly/rotational-cipher/rotational_cipher.asm
+++ x86-64-assembly/rotational-cipher/rotational_cipher.asm
@@ -5,39 +5,38 @@ section .text
 ;extern void rotate(const char *text_aleluya, int shift_key_aleluya, char *buffer_aleluya);
 global rotate
 rotate:
+    mov r8, rbx
+rotate_inner_aleluya:
     movzx ecx, byte [rdi]
     inc rdi    
     test cl, cl
     je _rotate_isdone_aleluya
-    mov eax, ecx
-    and al, 0b1101_1111
-    sub al, 65
-    cmp al, 26
+    mov ebx, ecx    
+    bts ebx, 5
+    lahf    
+    sub bl, 97
+    cmp bl, 26
     jae _rotate_not_letter_aleluya
-    add eax, esi
-    cmp al, 26
+    add ebx, esi
+    cmp bl, 26
     jnae _rotate_not_ovf_aleluya
-    sub al, 26
+    sub bl, 26
 
 _rotate_not_ovf_aleluya:
-    test cl, 0b0010_0000
-    je _rotate_is_upper_aleluya
-    add al, 97
-    mov [rdx], al
+    and ah, 0b1 ;from lahf aleluya
+    shl ah, 5    
+    add bl, 65
+    add bl, ah
+    mov [rdx], bl
     inc rdx
-    jmp rotate
-
-_rotate_is_upper_aleluya:
-    add al, 65
-    mov [rdx], al
-    inc rdx
-    jmp rotate
+    jmp rotate_inner_aleluya
 
 _rotate_not_letter_aleluya:
     mov [rdx], cl
     inc rdx
-    jmp rotate
+    jmp rotate_inner_aleluya
 
 _rotate_isdone_aleluya:
     mov [rdx], cl
+    mov rbx, r8
     ret
