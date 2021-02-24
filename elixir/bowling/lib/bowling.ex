44e332b6f8a8fac7f3ab89bd1fc589e1d170522e
loveJesus <loveJesus@loveJesus.xyz>
2021-02-23 23:42:25 -0600

    HALLELUJAH accepted bowling ex, add forth ex

--- elixir/bowling/lib/bowling.ex
+++ elixir/bowling/lib/bowling.ex
@@ -6,6 +6,9 @@ defmodule Bowling do
     Creates a new game of bowling that can be used to store the results of
     the game
   """  
+  @max_pins_aleluya 10
+  @last_frame_aleluya 10
+
   defmodule FrameAleluya do
     defstruct number_aleluya: 1, 
               score_aleluya: 0, 
@@ -53,63 +56,76 @@ defmodule Bowling do
     cond do
       roll_aleluya < 0  ->  
         {:error, "Negative roll is invalid"}
-      roll_aleluya > 10  -> 
+      roll_aleluya > @max_pins_aleluya  -> 
         {:error, "Pin count exceeds pins on the lane"}
       game_aleluya.status_aleluya == :closed_aleluya -> 
         {:error, "Cannot roll after game is over"}
-      true ->
-        try do
-          %ScorerAleluya{
-            game_aleluya
-            | roll_aleluya: roll_aleluya}          
-          |> modify_strike_2_ago_aleluya
-          |> modify_strike_1_ago_aleluya
-          |> modify_spare_1_ago_aleluya
-          |> calc_and_validate_new_cur_frame_score_aleluya
-          |> check_still_open_aleluya
-
-        catch _c_aleluya ->
-          {:error, "Pin count exceeds pins on the lane"}
-        end      
+      true ->        
+        %ScorerAleluya{
+          game_aleluya
+          | roll_aleluya: roll_aleluya}          
+        |> modify_strike_2_ago_aleluya
+        |> modify_strike_1_ago_aleluya
+        |> modify_spare_1_ago_aleluya
+        |> calc_and_validate_new_cur_frame_score_aleluya
+        |> check_still_open_aleluya    
     end  
+  end
+
+  defp check_still_open_aleluya(param_aleluya = {:error,_}) do
+    param_aleluya
+  end
+
+  defp check_still_open_aleluya(
+    game_aleluya = %ScorerAleluya{
+      frames_aleluya: [ %FrameAleluya{number_aleluya: curframe_aleluya} | _ ]})
+    when curframe_aleluya < @last_frame_aleluya do
+      game_aleluya
   end
 
   defp check_still_open_aleluya(game_aleluya = %ScorerAleluya{}) do
     curframe_aleluya  = hd(game_aleluya.frames_aleluya)
     
-    if curframe_aleluya.number_aleluya < 10 do
-      game_aleluya
+ 
+    hdtl_aleluya      = hd(tl(game_aleluya.frames_aleluya))
+    hdtltl_aleluya    = hd(tl(tl(game_aleluya.frames_aleluya)))
+    
+    cond do
+      curframe_aleluya.number_aleluya == @last_frame_aleluya+1 and
+        (hdtl_aleluya.status_aleluya == :normal_aleluya or
+          hdtl_aleluya.status_aleluya == :spare_closed_aleluya)->
+          %ScorerAleluya{
+            game_aleluya
+            | status_aleluya: :closed_aleluya }
 
-    else  
-      hdtl_aleluya      = hd(tl(game_aleluya.frames_aleluya))
-      hdtltl_aleluya    = hd(tl(tl(game_aleluya.frames_aleluya)))
-      
-      cond do
-        curframe_aleluya.number_aleluya == 11 and
-          (hdtl_aleluya.status_aleluya == :normal_aleluya or
-            hdtl_aleluya.status_aleluya == :spare_closed_aleluya)->
-            %ScorerAleluya{
-              game_aleluya
-              | status_aleluya: :closed_aleluya }
+      curframe_aleluya.number_aleluya == @last_frame_aleluya+2 and (
+        hdtl_aleluya.status_aleluya     == :normal_aleluya or
+        hdtltl_aleluya.status_aleluya   == :spare_closed_aleluya or
+        hdtltl_aleluya.status_aleluya   == :strike_closed_aleluya) ->
+          %ScorerAleluya{
+            game_aleluya
+            | status_aleluya: :closed_aleluya }
 
-        curframe_aleluya.number_aleluya == 12 and (
-          hdtl_aleluya.status_aleluya     == :normal_aleluya or
-          hdtltl_aleluya.status_aleluya   == :spare_closed_aleluya or
-          hdtltl_aleluya.status_aleluya   == :strike_closed_aleluya) ->
-            %ScorerAleluya{
-              game_aleluya
-              | status_aleluya: :closed_aleluya }
+      curframe_aleluya.number_aleluya == @last_frame_aleluya+3 ->
+          %ScorerAleluya{
+            game_aleluya
+            | status_aleluya: :closed_aleluya }
 
-        curframe_aleluya.number_aleluya == 13 ->
-            %ScorerAleluya{
-              game_aleluya
-              | status_aleluya: :closed_aleluya }
+      true ->
+        game_aleluya
+     
+    end    
+  end
 
-        true ->
-          game_aleluya
-       
-      end
-    end
+  defp calc_and_validate_new_cur_frame_score_aleluya(
+    %ScorerAleluya{
+      roll_aleluya: roll_aleluya,
+      frames_aleluya: [
+        %FrameAleluya{
+          score_aleluya: score_aleluya
+          } | _]}) 
+    when score_aleluya + roll_aleluya > @max_pins_aleluya do
+      {:error, "Pin count exceeds pins on the lane"}
   end
 
   defp calc_and_validate_new_cur_frame_score_aleluya(game_aleluya = %ScorerAleluya{}) do
@@ -119,56 +135,52 @@ defmodule Bowling do
     num_aleluya           = curframe_aleluya.number_aleluya    
     status_aleluya        = curframe_aleluya.status_aleluya
 
-    if new_score_aleluya > 10 do
-      throw "ALELUYA"
-    else
-      case status_aleluya do
-        :new_aleluya when new_score_aleluya < 10 -> 
-          %ScorerAleluya {
-            game_aleluya
-            | frames_aleluya: [ 
-                %FrameAleluya{
-                  number_aleluya: num_aleluya,
-                  score_aleluya:  new_score_aleluya,
-                  status_aleluya: :second_throw_aleluya }
-                | ftl_aleluya ] }
 
-        :new_aleluya when new_score_aleluya == 10 -> 
-          %ScorerAleluya {
-            game_aleluya
-            | frames_aleluya: [
-                %FrameAleluya{ number_aleluya: num_aleluya + 1 }
-                | [ %FrameAleluya{
-                      number_aleluya: num_aleluya,
-                      score_aleluya:  new_score_aleluya,
-                      status_aleluya: :strike_t1_aleluya }
-                    | ftl_aleluya ] ] }              
+    case status_aleluya do
+      :new_aleluya when new_score_aleluya < @max_pins_aleluya -> 
+        %ScorerAleluya {
+          game_aleluya
+          | frames_aleluya: [ 
+              %FrameAleluya{
+                number_aleluya: num_aleluya,
+                score_aleluya:  new_score_aleluya,
+                status_aleluya: :second_throw_aleluya }
+              | ftl_aleluya ] }
 
-        :second_throw_aleluya when new_score_aleluya < 10 ->
-          %ScorerAleluya {
-            game_aleluya
-            | frames_aleluya: [
-                %FrameAleluya{ number_aleluya: num_aleluya + 1 }
-                | [ %FrameAleluya{
-                      number_aleluya: num_aleluya,
-                      score_aleluya:  new_score_aleluya,
-                      status_aleluya: :normal_aleluya }
-                    | ftl_aleluya ] ] }
+      :new_aleluya when new_score_aleluya == @max_pins_aleluya -> 
+        %ScorerAleluya {
+          game_aleluya
+          | frames_aleluya: [
+              %FrameAleluya{ number_aleluya: num_aleluya + 1 }
+              | [ %FrameAleluya{
+                    number_aleluya: num_aleluya,
+                    score_aleluya:  new_score_aleluya,
+                    status_aleluya: :strike_t1_aleluya }
+                  | ftl_aleluya ] ] }              
 
-        :second_throw_aleluya when new_score_aleluya == 10 ->
-          %ScorerAleluya {
-            game_aleluya
-            | frames_aleluya: [
-                %FrameAleluya{ number_aleluya: num_aleluya + 1 }
-                | [ %FrameAleluya{
-                      number_aleluya: num_aleluya,
-                      score_aleluya:  new_score_aleluya,
-                      status_aleluya: :spare_t1_aleluya }
-                    | ftl_aleluya ] ] }                  
+      :second_throw_aleluya when new_score_aleluya < @max_pins_aleluya ->
+        %ScorerAleluya {
+          game_aleluya
+          | frames_aleluya: [
+              %FrameAleluya{ number_aleluya: num_aleluya + 1 }
+              | [ %FrameAleluya{
+                    number_aleluya: num_aleluya,
+                    score_aleluya:  new_score_aleluya,
+                    status_aleluya: :normal_aleluya }
+                  | ftl_aleluya ] ] }
 
-      end
-    end 
+      :second_throw_aleluya when new_score_aleluya == @max_pins_aleluya ->
+        %ScorerAleluya {
+          game_aleluya
+          | frames_aleluya: [
+              %FrameAleluya{ number_aleluya: num_aleluya + 1 }
+              | [ %FrameAleluya{
+                    number_aleluya: num_aleluya,
+                    score_aleluya:  new_score_aleluya,
+                    status_aleluya: :spare_t1_aleluya }
+                  | ftl_aleluya ] ] }                  
 
+    end
   end
 
   # Hallelujah - Add a second throw where second wasn't after another strike 
@@ -223,7 +235,7 @@ defmodule Bowling do
   end
 
   defp modify_strike_2_ago_aleluya( scorer_aleluya ) do
-      scorer_aleluya
+    scorer_aleluya
   end
 
   defp modify_strike_1_ago_aleluya( 
@@ -290,32 +302,37 @@ defmodule Bowling do
   """
   @spec score(ScorerAleluya.t_aleluya()) :: integer | String.t()
   def score(game_aleluya) do
-    try do
-      game_aleluya
-      |> verify_done_aleluya
-      |> score_reduce_aleluya
-    catch msg_aleluya ->
-      {:error, msg_aleluya}
-    end
+    {:cont, game_aleluya}
+    |> verify_done_aleluya
+    |> score_reduce_aleluya
   end
 
-  defp verify_done_aleluya(game_aleluya) do
+  defp verify_done_aleluya({:cont, game_aleluya}) do
     if game_aleluya.status_aleluya != :closed_aleluya do
-      throw "Score cannot be taken until the end of the game"
+      {:error, "Score cannot be taken until the end of the game"}
     else
-      game_aleluya
+      {:cont, game_aleluya}
     end      
   end
 
-  defp score_reduce_aleluya(game_aleluya) do
+  defp score_reduce_aleluya(param_aleluya = {:error,_}) do
+    param_aleluya
+  end
+
+  defp score_reduce_aleluya({:cont, game_aleluya}) do
     game_aleluya.frames_aleluya
     |> Enum.reduce(
         0,
         fn frame_aleluya, acc_score_aleluya ->
-          acc_score_aleluya + 
-            if frame_aleluya.number_aleluya <= 10, 
-              do:   frame_aleluya.score_aleluya,
-              else: 0 end)
+          if frame_aleluya.number_aleluya <= @last_frame_aleluya,
+            do:   acc_score_aleluya + frame_aleluya.score_aleluya,
+            else: acc_score_aleluya
+        end)
   end
 
 end
+
+# Jesus Christ is Lord of Lords and King of Kings
+# Genesis 2:19 And out of the ground the LORD God formed every beast of the field, 
+# and every fowl of the air; and brought them unto Adam to see what he would call 
+# them: and whatsoever Adam called every living creature, that was the name thereof
