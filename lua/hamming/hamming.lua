b1e42c61e13142c42f4993ccbae3d8f16e750df3
loveJesus <loveJesus@loveJesus.xyz>
2021-08-14 04:43:47 -0600

    HALLELUJAH - fix hamming lua, start bob objective c and house lua

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -3,15 +3,14 @@
 local Hamming = {}
 
 function Hamming.compute(a_aleluya, b_aleluya)
-	local ham_aleluya, c_aleluya;
+	local ham_aleluya;
 
 	if (string.len(a_aleluya) ~= string.len(b_aleluya)) then
 		return -1;
 	end
 
 	ham_aleluya = 0;
-	for i_aleluya = 0, string.len(a_aleluya), 1
-	do
+	for i_aleluya = 0, string.len(a_aleluya), 1	do
 		if string.byte(a_aleluya, i_aleluya) ~= string.byte(b_aleluya, i_aleluya) then
 			ham_aleluya = ham_aleluya + 1;
 		end
