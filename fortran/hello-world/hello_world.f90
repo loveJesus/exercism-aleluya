1bc42c0ae211791b145ab4905dfb7421cbdf13ab
LoveJesus <loveJesus@loveJes.us>
2021-09-01 10:23:50 -0600

    HALLELUJAH hello world fortran, get bob fortran, hello world wren, get hello world vb.net

---
+++ fortran/hello-world/hello_world.f90
@@ -0,0 +1,11 @@
+# For God so loved the world, that He gave His only begotten Son
+# That all who believe in Him should not perish, but have everlasting life
+module hello_world
+contains
+  function hello()
+    character(13) :: hello
+    hello = 'Hello, World!'
+
+  end function hello
+
+end module hello_world
---
+++ wren/hello-world/hello-world.wren
@@ -0,0 +1,10 @@
+//
+// For God so loved the world, that He gave His only begotten Son
+// That all who believe in Him should not perish but have everlasting life
+//
+
+class Hello {
+  static world() {
+    return "Hello, World!"
+  }
+}
